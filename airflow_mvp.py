c0872ec44eccbb046ff2d75f3491dfd18baf0b76
tekurioptions <31213645+tekurioptions@users.noreply.github.com>
2019-04-24 14:27:18 +0200

    Add files via upload

--- airflow_mvp.py
+++ airflow_mvp.py
@@ -2,46 +2,49 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import timedelta, datetime
+from airflow_core.airflow_helper import save_df_as_csv_to_s3, upload_file_to_s3
+from airflow_core.matched_helper import read_files_from_s3
 import boto3
 import pandas as pd
 import io
 from io import StringIO
 
-author_columns = ['author_id', 'first_name', 'middle_name', 'lastname', 'coauthor_lastnames',
-                      'topics', 'cities', 'countries', 'orcid']
-investigator_columns = ['investigator_id', 'first_name', 'middle_name', 'lastname', 'coinvestigator_lastnames',
-                        'topics', 'cities', 'countries', 'orcid']
-output_columns = ['author_id', 'investigator_id']
 
-s3 = boto3.resource('s3')
+# author_columns = ['author_id', 'first_name', 'middle_name', 'lastname', 'coauthor_lastnames',
+#                       'topics', 'cities', 'countries', 'orcid']
+# investigator_columns = ['investigator_id', 'first_name', 'middle_name', 'lastname', 'coinvestigator_lastnames',
+#                         'topics', 'cities', 'countries', 'orcid']
+# output_columns = ['author_id', 'investigator_id']
 
-def s3_client():
-    return boto3.client('s3')
+# s3 = boto3.resource('s3')
 
-def upload_file_to_s3(filename, key, bucket_name):
-    s3.Bucket(bucket_name).upload_file(filename, key)
+# def s3_client():
+#     return boto3.client('s3')
 
-def read_csv_from_s3(bucket_name, input_key, columns):
-    obj = s3_client().get_object(Bucket= bucket_name , Key = input_key)
-    df = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='Windows-1252')
-    df.columns = columns
-    return df.fillna('')
+# def upload_file_to_s3(filename, key, bucket_name):
+#     s3.Bucket(bucket_name).upload_file(filename, key)
 
-def save_df_as_csv_to_s3(df, bucket_name, output_key):
-    csv_buffer = StringIO()
-    df.to_csv(csv_buffer)
-    s3.Object(bucket_name, output_key).put(Body=csv_buffer.getvalue())
+# def read_csv_from_s3(bucket_name, input_key, columns):
+#     obj = s3_client().get_object(Bucket= bucket_name , Key = input_key)
+#     df = pd.read_csv(io.BytesIO(obj['Body'].read()), encoding='Windows-1252')
+#     df.columns = columns
+#     return df.fillna('')
 
-def get_matched_by_orc_id(authors, investigators):
-    result = pd.merge(authors, investigators, on='orcid')
-    result = result[output_columns]
-    return result
+# def save_df_as_csv_to_s3(df, bucket_name, output_key):
+#     csv_buffer = StringIO()
+#     df.to_csv(csv_buffer)
+#     s3.Object(bucket_name, output_key).put(Body=csv_buffer.getvalue())
 
-def read_files_from_s3(bucket_name, key1, key2):
-    df1 = read_csv_from_s3(bucket_name, key1, author_columns)
-    df2 = read_csv_from_s3(bucket_name, key2, investigator_columns)
-    return get_matched_by_orc_id(df1[(df1['orcid'].notnull()) & (df1['orcid'] != '')],
-                                 df2[(df2['orcid'].notnull()) & (df2['orcid'] != '')])
+# def get_matched_by_orc_id(authors, investigators):
+#     result = pd.merge(authors, investigators, on='orcid')
+#     result = result[output_columns]
+#     return result
+
+# def read_files_from_s3(bucket_name, key1, key2):
+#     df1 = read_csv_from_s3(bucket_name, key1, author_columns)
+#     df2 = read_csv_from_s3(bucket_name, key2, investigator_columns)
+#     return get_matched_by_orc_id(df1[(df1['orcid'].notnull()) & (df1['orcid'] != '')],
+#                                  df2[(df2['orcid'].notnull()) & (df2['orcid'] != '')])
 
 def read_transform_save(bucket_name, input_key1, input_key2, output_key):
     output = read_files_from_s3(bucket_name, input_key1, input_key2)
