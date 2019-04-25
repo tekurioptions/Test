63bd67fbb46dc5e7abd34f85c046db22adffb4bf
tekurioptions <31213645+tekurioptions@users.noreply.github.com>
2019-04-25 11:49:32 +0200

    Add files via upload

--- kwargs_test.py
+++ kwargs_test.py
@@ -1,44 +1,53 @@
-from __future__ import print_function
-import airflow
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import timedelta, datetime
 
-args = {
-    'owner': 'airflow',
-    'start_date': airflow.utils.dates.days_ago(2)
+SCHEDULE_INTERVAL = '@once'
+
+DAG_DEFAULT_ARGS = {
+        'owner': 'Nagendra',
+        'start_date': datetime(2019, 4, 24),
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
 }
 
-dag = DAG(
-    'example_xcom',
-    schedule_interval="@once",
-    default_args=args)
+DAG_VERSION = 'xcom_example_1.0'
 
+dag = DAG(DAG_VERSION,
+          default_args=DAG_DEFAULT_ARGS,
+          schedule_interval=SCHEDULE_INTERVAL,
+          concurrency=1,
+          max_active_runs=1,
+          catchup=False)
 
-def push(**kwargs):
+def data_passed(**kwargs):
     task_instance = kwargs['ti']
-    value_1 = [1, 2, 3, 4]
-    task_instance.xcom_push(key='key1', value=value_1)
+    list_passed = [1,2,3]
+    task_instance.xcom_push(key='my_data', value=list_passed)
 
-
-def puller(**kwargs):
-    ti = kwargs['ti']
-    v1 = ti.xcom_pull(key='key1', task_ids='push')
-    print(v1)
-    return v1
+def data_received(**kwargs):
+    task_instance = kwargs['ti']
+    my_list = task_instance.xcom_pull(key='my_data', task_ids="sending_task")
+    print(my_list)
+    return my_list
 
 start_task = DummyOperator(task_id='start_dummy_task', retries=3, dag=dag)
 
-push1 = PythonOperator(
-    task_id='push', dag=dag, python_callable=push, provide_context=True)
+sending_task = PythonOperator(task_id='sending_task',
+                              python_callable=data_passed,
+                              retries=0,
+                              provide_context=True,
+                              dag=dag)
 
-pull = BashOperator(
-    task_id='also_run_this',
-    bash_command='echo {{ ti.xcom_pull(task_ids="push") }}',
-    dag=dag)
+receiving_task = PythonOperator(task_id='receiving_task',
+                              python_callable=data_received(),
+                              retries=0,
+                              provide_context=True,
+                              dag=dag)
+end_task = DummyOperator(task_id='end_dummy_task', retries=3, dag=dag)
 
-pull2 = PythonOperator(
-    task_id='pull2', dag=dag, python_callable=puller, provide_context=True)
-
-start_task >> push1 >> pull >> pull2
+start_task >> sending_task >> receiving_task >> end_task
