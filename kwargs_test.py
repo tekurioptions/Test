2b46ac268b657924cb2ba52e76ce3487773e7e7e
tekurioptions <31213645+tekurioptions@users.noreply.github.com>
2019-04-25 10:41:34 +0200

    Add files via upload

--- kwargs_test.py
+++ kwargs_test.py
@@ -19,21 +19,21 @@ value_1 = [1, 2, 3]
 
 def push(**kwargs):
     # pushes an XCom without a specific target
-    kwargs['ti'].xcom_push(key='value from pusher 1', value=value_1)
+    kwargs['ti'].xcom_push(key='key1', value=value_1)
 
 
 def puller(**kwargs):
     ti = kwargs['ti']
-
-    v1 = ti.xcom_pull(key=None, task_ids='push')
-    assert v1 == value_1
+    v1 = ti.xcom_pull(key='key1', task_ids='push')
+    print(v1)
+    return v1
 
 push1 = PythonOperator(
     task_id='push', dag=dag, python_callable=push, provide_context=True)
 
 pull = BashOperator(
     task_id='also_run_this',
-    bash_command='echo {{ ti.xcom_pull(task_ids="push_by_returning") }}',
+    bash_command='echo {{ ti.xcom_pull(task_ids="push") }}',
     dag=dag)
 
 # pull.set_upstream(push1)
